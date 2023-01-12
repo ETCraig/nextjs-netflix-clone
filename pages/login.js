a87ab0c90e1b308cd8120477d8b5c7d0b710db55
Craig <dev.etcraig@gmail.com>
2023-01-11 23:59:50 -0700

    Lowercase

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { magic } from "../lib/magic-client";
 
-import styles from "../styles/Login.module.css"
+import styles from "../styles/login.module.css"
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [userMsg, setUserMsg] = useState("");
