396a5570435c8f15e74d08ecd261e0d80ed09d2a
Craig <dev.etcraig@gmail.com>
2023-01-12 00:34:55 -0700

    Added to document

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&family=Roboto+Slab&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
