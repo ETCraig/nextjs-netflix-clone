213bec3db49ab5dcba835bd157266752175fff48
Craig <dev.etcraig@gmail.com>
2023-01-11 19:04:04 -0700

    GraphQL Functions for findVideoIdByUser & updateStats Created - Stats API Logic Extended

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,40 @@
+import jwt from "jsonwebtoken";
+import {
+  findVideoIdByUser,
+  insertStats,
+  updateStats,
+} from "../../lib/db/hasura";
+
 export default async function stats(req, resp) {
   if (req.method === "POST") {
-    resp.send({ msg: "Stats API" });
+    console.log({ cookies: req.cookies });
+
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        resp.status(403).send({});
+      } else {
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log({ decoded });
+        const userId = decodedToken.issuer;
+
+        const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
+        if (doesStatsExist) {
+          const response = await updateStats(token, {
+            watched: true,
+            userId,
+            videoId: "gxc6y2ZVfCU",
+            favourited: 0,
+          });
+          resp.send({ msg: "it works", response });
+        } else {
+          resp.send({ msg: "it works", decodedToken, doesStatsExist });
+        }
+      }
+    } catch (error) {
+      console.error("Error occurred /stats", error);
+      resp.status(500).send({ done: false, error: error?.message });
+    }
   }
 }
